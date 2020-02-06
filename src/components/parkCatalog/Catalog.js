b5b7ed4ee3ef15fb54c9f692d0ac26e04fa1c8ee
Alejoguzm07 <alejoguzm07@gmail.com>
2020-02-06 15:30:30 -0500

    Branch creation

--- src/components/parkCatalog/Catalog.js
+++ src/components/parkCatalog/Catalog.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
-import GridList from '@material-ui/core/GridList';
+import { GridList } from '@material-ui/core';
 import GridListTile from '@material-ui/core/GridListTile';
 import { Search } from './Search';
 import { ParkCard } from './park/ParkCard';
@@ -15,7 +15,7 @@ export class Catalog extends React.Component {
         return (
             <div>
                 <Search />
-                <GridList>
+                <GridList cols={4} spacing={1}>
                     {this.state.cards.map(card => (
                         <GridListTile >
                             <ParkCard />
--- src/components/parkCatalog/park/ParkCard.js
+++ src/components/parkCatalog/park/ParkCard.js
@@ -1,17 +1,25 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import SentimentVeryDissatisfiedIcon from '@material-ui/icons/SentimentVeryDissatisfied';
+import SentimentDissatisfiedIcon from '@material-ui/icons/SentimentDissatisfied';
+import SentimentSatisfiedIcon from '@material-ui/icons/SentimentSatisfied';
+import SentimentSatisfiedAltIcon from '@material-ui/icons/SentimentSatisfiedAltOutlined';
+import SentimentVerySatisfiedIcon from '@material-ui/icons/SentimentVerySatisfied';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import ButtonBase from '@material-ui/core/ButtonBase';
 
 export class ParkCard extends React.Component {
+    
     render() {
 
         return (
             <div >
-                <Paper >
-                    <Grid container spacing={2}>
+                <Paper elevation={3}>
+                    <Grid container spacing={3}>
                         <Grid item>
                             <ButtonBase >
                                 <img alt="complex" src="/static/images/grid/complex.jpg" />
@@ -21,19 +29,23 @@ export class ParkCard extends React.Component {
                             <Grid item xs container direction="column" spacing={2}>
                                 <Grid item xs>
                                     <Typography gutterBottom variant="subtitle1">
-                                        Standard license
-                                    </Typography>
-                                    <Typography variant="body2" gutterBottom>
-                                        Full resolution 1920x1080 • JPEG
+                                        Park Name
                                     </Typography>
                                     <Typography variant="body2" color="textSecondary">
                                         ID: 1030114
                                     </Typography>
+                                    <Typography variant="body2" gutterBottom>
+                                        Description
+                                    </Typography>
+                                    
                                 </Grid>
                                 <Grid item>
-                                    <Typography variant="body2" style={{ cursor: 'pointer' }}>
-                                        Remove
-                                    </Typography>
+                                <Rating
+                                    name="customized-icons"
+                                    defaultValue={2}
+                                    getLabelText={value => customIcons[value].label}
+                                    IconContainerComponent={IconContainer}
+                                />
                                 </Grid>
                             </Grid>
                             <Grid item>
@@ -46,3 +58,46 @@ export class ParkCard extends React.Component {
         );
     }
 }
+
+const StyledRating = withStyles({
+    iconFilled: {
+      color: '#ff6d75',
+    },
+    iconHover: {
+      color: '#ff3d47',
+    },
+  })(Rating);
+  
+  const customIcons = {
+    1: {
+      icon: <SentimentVeryDissatisfiedIcon />,
+      label: 'Very Dissatisfied',
+    },
+    2: {
+      icon: <SentimentDissatisfiedIcon />,
+      label: 'Dissatisfied',
+    },
+    3: {
+      icon: <SentimentSatisfiedIcon />,
+      label: 'Neutral',
+    },
+    4: {
+      icon: <SentimentSatisfiedAltIcon />,
+      label: 'Satisfied',
+    },
+    5: {
+      icon: <SentimentVerySatisfiedIcon />,
+      label: 'Very Satisfied',
+    },
+  };
+  
+  function IconContainer(props) {
+    const { value, ...other } = props;
+    return <span {...other}>{customIcons[value].icon}</span>;
+  }
+  
+  IconContainer.propTypes = {
+    value: PropTypes.number.isRequired,
+  };
+  
+  
