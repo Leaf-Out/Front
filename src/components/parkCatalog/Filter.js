cc12d78d744201b0a68581ac7f2bc2b1bf9b8247
luis <luis.moreno-a@mail.escuelaing.edu.co>
2020-02-16 21:34:35 -0500

    Use of chips instead of navigation with icons. Use of popper for show filters. Missing the filter content

--- src/components/parkCatalog/Filter.js
+++ src/components/parkCatalog/Filter.js
@@ -1,37 +1,79 @@
-import React from 'react';
-import ChildCareIcon from '@material-ui/icons/ChildCare';
-import Icon from '@material-ui/core/Icon';
-import RowingIcon from '@material-ui/icons/Rowing';
-import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
-import TextField from '@material-ui/core/TextField';
-import { BottomNavigation } from '@material-ui/core';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-
+import React from "react";
+import ChildCareIcon from "@material-ui/icons/ChildCare";
+import Icon from "@material-ui/core/Icon";
+import RowingIcon from "@material-ui/icons/Rowing";
+import EmojiPeopleIcon from "@material-ui/icons/EmojiPeople";
+import TextField from "@material-ui/core/TextField";
+import { BottomNavigation, Popper, Chip, Divider } from "@material-ui/core";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
 
 export class Filter extends React.Component {
-    constructor(props){
-        super(props)
-        this.state = {
-            selected: 'childs'
-        }
-    }
-    render() {
-        return (
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+      selected: "childs",
+      poperContent: "Filter Here"
+    };
+  }
+  handleChange = (event, newValue) => {
+    this.setState({
+      selected: newValue
+    });
+  };
+
+  handleClick = (event) => {
+    this.setState(prevState => ({
+      anchorEl: prevState.anchorEl ? null : event.currentTarget
+    }));
+  };
+
+  filterByVisitors = () => {
+    this.setState({
+        poperContent:(
             <div>
-                <BottomNavigation value={this.state.selected} onChange={this.handleChange}>
-                    <BottomNavigationAction label="childs" value="childs" icon={<ChildCareIcon />} />
-                    <BottomNavigationAction label="adults" value="adults" icon={<EmojiPeopleIcon />} />
-                    <BottomNavigationAction label="location" value="location" icon={<LocationOnIcon />} />
-                    <BottomNavigationAction label="activities" value="activities" icon={<RowingIcon />} />
-                </BottomNavigation>
+                <TextField type="number"> Adults</TextField>
+                <TextField type="number"> Childs</TextField>
             </div>
         )
-    }
+    })
+  };
 
-    handleChange = (event, newValue) => {
-        this.setState( {
-            selected: newValue
-        });
-      }
+  filterByList = (Category) => {
+    this.setState({poperContent:(<div></div>)})
+  };
+  filterByActivities = () => {};
+  render() {
+    const content = (<div> {this.state.poperContent} </div>);
+    const open = Boolean(this.state.anchorEl);
+    return (
+      <div>
+        <Popper
+          open={open}
+          anchorEl={this.state.anchorElanchorEl}
+          placement="bottom"
+        >
+          {content}
+        </Popper>
+        <Chip
+          icon={<EmojiPeopleIcon />}
+          label="Visitors"
+          onClick={this.handleClick}
+        />
+        <Chip
+          icon={<LocationOnIcon />}
+          label="Location"
+          onClick={this.handleClick}
+        />
+        <Chip
+          icon={<RowingIcon />}
+          label="Activities"
+          onClick={this.handleClick}
+        />
+
+        <Chip icon={<RowingIcon />} label="More" onClick={this.handleClick} />
+      </div>
+    );
+  }
 }
