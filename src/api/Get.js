99150c697e3af25d10f7712b39dc38b0a4dcbf50
luis <luis.moreno-a@mail.escuelaing.edu.co>
2020-05-05 19:02:22 -0500

    post request

--- src/api/Get.js
+++ src/api/Get.js
@@ -7,7 +7,7 @@ export const login = (name, pass) => {
     let requestUrl = `${url}/login?username=${name}&password=${pass}`
 
     return new Promise((resolve, reject) => {
-        axios.get(requestUrl,{ "headers": { "Authorization": "Bearer " + localStorage.getItem("token") } })
+        axios.get(requestUrl,{ "headers": { "Authorization": "Bearer "} })
         .then(res => {
             resolve(res);
         }).catch(err => {
@@ -18,10 +18,24 @@ export const login = (name, pass) => {
 
 export const get = (apiPath)=>{
     let requestUrl = `${url}${apiPath}`;
-    console.log(url);
+    let bearer = localStorage.getItem("token")
     
     return new Promise((resolve, reject) => {
-        axios.get(requestUrl,{ "headers": { "Authorization": "Bearer " + localStorage.getItem("token") } })
+        axios.get(requestUrl,{ "headers": { "Authorization": `Bearer ${bearer}` } })
+        .then(res => {
+            resolve(res.data);
+        }).catch(err => {
+            reject(err)
+        });
+    });
+}
+
+export const post = (path, data)=>{
+    let requestUrl = `${url}${path}`;    
+    return new Promise((resolve, reject) => {
+        axios.post(requestUrl,
+             data,
+             { "headers": { "Authorization": "Bearer " + localStorage.getItem("token") } })
         .then(res => {
             resolve(res.data);
         }).catch(err => {
