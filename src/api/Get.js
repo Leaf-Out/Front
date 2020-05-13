f1e6614d5f89c84fca0c2271b66c455cde39dc4d
alejoguzm07 <josealejandro.naranjo@payulatam.com>
2020-05-12 22:22:08 -0500

    Production url

--- src/api/Get.js
+++ src/api/Get.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//const url = 'https://leaf-out.herokuapp.com'
-const url = 'http://localhost:8080'
+const url = 'https://leaf-out.herokuapp.com'
 
 export const login = (name, pass) => {
 
